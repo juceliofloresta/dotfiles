e20208b49c6a4ae63b41ce7beacb7f0eb8ba9817
Jucelio Floresta <jucelio.floresta@gmail.com>
2021-01-10 20:28:05 -0300

    feat: add colours name to normalize with base16

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,38 +2,17 @@
 set -U NVIM_TUI_ENABLE_TRUE_COLOR 1
 set -U EDITOR nvim
 
-set -U WATSON_DIR ~/.watson
-
-# asdf
-source /usr/local/opt/asdf/asdf.fish
+source ~/.asdf/asdf.fish
 
 # Alias
-alias tree "tree -I \"node_modules|vendor|bower_components\""
-alias cpwd "pwd | pbcopy"
-alias editfish "nvim ~/.config/fish/config.fish"
 alias mux "tmux -2 -u new -A -s main"
-alias vim="nvim"
-alias vimdiff="nvim -d"
-alias doc="cd ~/Documents"
-alias down="cd ~/Downloads"
-alias app="cd ~/Applications"
-alias work="cd ~/Workspace"
+alias today "nvim ~/documents/today.adoc"
+alias stone "cd ~/desktop/stone"
+alias vim "nvim"
 
-# Alias - Git
-alias g "git"
-alias gs "git status"
-alias gd "git diff"
-alias gda "git diff --staged"
-alias gc "git commit"
-alias gck "git checkout"
-alias gsw "git switch"
-alias gr "git restore"
-alias gl "git log --all --decorate --oneline --graph"
-alias ga "git add"
-alias gap "git add --patch"
-alias gbm "git branch --merge"
-alias gbd "git branch -d"
-alias gbdf "git branch -D"
+# fish
+alias reload_fish "source ~/.config/fish/config.fish"
+alias edit_fish "nvim ~/.config/fish/config.fish"
 
 # Prompt
 function fish_prompt
@@ -45,6 +24,6 @@ end
 
 # Base16 Shell
 if status --is-interactive
-	set -U BASE16_SHELL "$HOME/.config/base16/shell/"
-	source "$BASE16_SHELL/profile_helper.fish"
+    set BASE16_SHELL "$HOME/.config/base16-shell/"
+    source "$BASE16_SHELL/profile_helper.fish"
 end
